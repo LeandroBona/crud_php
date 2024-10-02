cb514f1a7102bfbe32ed2b5abe3abf1165815e37
Leandro Bona <42920804+LeandroBona@users.noreply.github.com>
2024-10-02 11:43:35 -0300

    Update update.php

--- update.php
+++ update.php
@@ -1,3 +1,14 @@
+<?php
+include 'config.php';
+
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM usuarios WHERE id=$id";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc(); //obtem os dados do registro
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
